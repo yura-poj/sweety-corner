34a8cb06b8fa4df91d0c9e532469261d0401ccdd
Yurii Pozhidaev <y.pozhidaev@g.nsu.ru>
2025-07-25 18:35:31 +0700

    renaming and adding views

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
   has_many :products, dependent: :nullify
 
-  validates :name, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: true
 end
--- db/migrate/20250723140752_create_products.rb
+++ db/migrate/20250723140752_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[8.0]
   def change
     create_table :products do |t|
-      t.string :name, null: false
+      t.string :title, null: false
       t.text :description
       t.decimal :price, null: false
       t.decimal :discount, null: false, default: 0
