e1e165edf3e5337d1d749823a94514d944cc460f
Yurii Pozhidaev <y.pozhidaev@g.nsu.ru>
2025-07-25 12:44:21 +0700

    edit migrates and models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many :cart_products
+
+  validates :name, :discount, :price, :available_quantity, presence: true
+
 end
--- db/migrate/20250723140752_create_products.rb
+++ db/migrate/20250723140752_create_products.rb
@@ -1,11 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[8.0]
   def change
     create_table :products do |t|
-      t.string :name
+      t.string :name, null: false
       t.text :description
-      t.decimal :price
-      t.integer :available_quantity
-
+      t.decimal :price, null: false
+      t.decimal :discount, null: false, default: 0
+      t.integer :available_quantity, null: false
       t.timestamps
     end
   end
