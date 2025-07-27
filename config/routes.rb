68903429ae1ebbc81b676fb8928c4b5134582c7b
Yurii Pozhidaev <y.pozhidaev@g.nsu.ru>
2025-07-27 21:53:37 +0700

    add category views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root "categories#index"
 
-  resources :products, only: [:show, :create, :update, :destroy, :new] do
+  resources :products, only: [:show, :create, :update, :destroy, :new, :edit] do
     get 'page/:page', action: :index, on: :collection
   end
 
-  resources :categories, only: [:index, :show, :create, :update, :destroy, :new]
+  resources :categories, only: [:index, :show, :create, :update, :destroy, :new, :edit]
 
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
