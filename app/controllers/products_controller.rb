f67e48a5fe4228fb23834eadca3658f3fa88ba6b
Yurii Pozhidaev <y.pozhidaev@g.nsu.ru>
2025-07-28 15:35:01 +0700

    add pagy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,6 @@
 class ProductsController < ApplicationController
-  PRODUCTS_PER_PAGE = 20
   def index
-    @products = Product.page(params[:page]).per(PRODUCTS_PER_PAGE)
+    @pagy, @products = pagy(Product.all)
   end
 
   def show
