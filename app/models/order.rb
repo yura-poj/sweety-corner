d06b161d2f69455f8bcb73337e60a393f1b59191
Yurii Pozhidaev <y.pozhidaev@g.nsu.ru>
2025-07-31 18:13:27 +0700

    add aasm

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,43 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
   
   validates :total_price, presence: true
+
+  aasm(:status, whiny_transitions: false) do
+    state :cart, initial: true
+    state :waiting_for_payment
+    state :payed
+    state :collecting
+    state :delivering
+    state :delivered
+    state :finished
+    state :canceled
+
+    event :cancel do
+      transitions to: :canceled
+    end
+
+    event :proceed do
+      transitions from: :cart, to: :waiting_for_payment
+    end
+
+    event :pay do
+      transitions from: :waiting_for_payment, to: :payed
+    end
+
+    event :wait_seller do
+      transitions from: :payed, to: :collecting
+    end
+
+    event :collect do
+      transitions from: :collecting, to: :delivering
+    end
+
+    event :deliver do
+      transitions from: :delivering, to: :delivered
+    end
+
+    event :finish do
+      transitions from: :delivered, to: :finished
+    end
+  end
 end
