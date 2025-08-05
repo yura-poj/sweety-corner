8864793c252f2c61b7f4278a939ebf72d35f7fdf
Yurii Pozhidaev <y.pozhidaev@g.nsu.ru>
2025-08-05 14:32:48 +0700

    add service

--- spec/factories/order_items.rb
+++ spec/factories/order_items.rb
@@ -2,25 +2,8 @@ FactoryBot.define do
   factory :order_item do
     quantity { 1 }
     total_price { 10.50 }
-    status { :pending }
+
     association :order
     association :product
-
-    trait :ordered do
-      status { :ordered }
-    end
-
-    trait :collecting do
-      status { :collecting }
-    end
-
-    trait :delivered do
-      status { :delivered }
-    end
-
-    trait :with_multiple_quantity do
-      quantity { 3 }
-      total_price { 31.50 }
-    end
   end
 end
