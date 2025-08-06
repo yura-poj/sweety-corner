bc2fc8d14bee8cbf346dd7f72fca5163b1a111a7
Yurii Pozhidaev <y.pozhidaev@g.nsu.ru>
2025-08-06 13:34:27 +0700

    add dry-monads

--- Gemfile
+++ Gemfile
@@ -56,6 +56,8 @@ gem "slim-rails"
 
 gem "aasm"
 
+gem 'dry-monads'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- app/services/order/product_adder.rb
+++ app/services/order/product_adder.rb
@@ -1,4 +1,5 @@
 class Order::ProductAdder
+  include Dry::Monads[:result]
 
   def initialize(order)
     @order = order
@@ -6,18 +7,33 @@ class Order::ProductAdder
 
   def call(product)
     @product = product
-    add_product
+    add_or_create_product
   end
 
   private
 
-  def add_product
+  def add_or_create_product
     @position = @order.order_items.find_by(product_id: @product)
 
-    @position ? @position.add : @order.order_items.create!(
-                                  product: @product,
-                                  quantity: 1,
-                                  total_price: @product.price
-                                )
+    @position ? add_product : create_product
+  end
+
+  def add_product
+    @position.add
+    Success(:ok)
+  end
+
+  def create_product
+    order = @order.order_items.create(
+      product: @product,
+      quantity: 1,
+      total_price: @product.price
+    )
+
+    if order
+      Success(:success)
+    else
+      Failure(:cannot_create_product)
+    end
   end
 end
